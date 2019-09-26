760cbae5ebbd723c0198eda3ec23c7f998064361
ZakStas <stanislavzakarliuka1989@gmail.com>
2019-09-26 23:29:56 +0300

    HW_03

--- JS/task_06.js
+++ JS/task_06.js
@@ -6,17 +6,20 @@ const products = [
   { name: 'Сканер', price: 2700, quantity: 3 },
   { name: 'Дроид', price: 400, quantity: 7 },
   { name: 'Захват', price: 1200, quantity: 2 },
+  { name: 'Радар', price: 1200, quantity: 5 },
 ];
 
 
+
 const calculateTotalPrice = function(arr, productName) {
+  let totalAmount = 0
   for (const obj of arr) {
     if(obj.name === productName){
       const total = obj.price * obj.quantity;
-      return total;
+      totalAmount += total
     }
   }
-
+  return totalAmount;
 }
 
 console.log(calculateTotalPrice(products, 'Радар')); // 5200
